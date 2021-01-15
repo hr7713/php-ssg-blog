0a32038c1afe1c6cde11c8c15ab2216f905e6d81
hr7713 <72539733+hr7713@users.noreply.github.com>
2021-01-15 17:44:51 +0900

    배포

---
+++ index.ssghtml.php
@@ -0,0 +1,155 @@
+<?php
+require_once "head.php";
+?>
+<link rel="stylesheet" href="css/index.css">
+<script src="js/index.js" defer></script>
+
+<section class="section-title con-min-width">
+  <h1 class="con">
+    <span>
+      <i class="fas fa-fire"></i>
+    </span>
+    <span>
+      LATEST ARTICLES
+    </span>
+  </h1>
+</section>
+
+
+<section class="section-latest-articles con-min-width">
+    <div class="con">
+      <div class="article-list-box">
+        <ul>
+          <li>
+            <h1>자바스크립트 사용법</h1>
+            <div>2021-01-12 12:12:14</div>
+            <script type="text/x-template">
+     # 자바스크립트 사용법
+```html
+<<!--REPLACE:script-->>
+var a =10;
+</<!--REPLACE:script-->>
+<div class="a"></div>
+```
+            </script>
+            <div class="toast-ui-viewer"></div>
+          </li>
+          <li>
+            <h1>자바스크립트 사용법</h1>
+            <div>2021-01-12 12:12:14</div>
+            <div>
+              <script type="text/x-template">
+                # HTML h1 사용법
+```codepen
+https://codepen.io/hr7713/embed/GRjYRGG?height=500&theme-id=light&default-tab=html,result
+```
+```youtube
+OMBjyeSJLqY
+```
+              </script>
+              <div class="toast-ui-viewer"></div>
+            </div>
+          </li>
+
+          <li>
+            <h1 class="title">2화, CSS DINER</h1>
+            <div class="reg-date">2020-01-12 12:12:14</div>
+            <div class="body">
+              <script type="text/x-template">
+# 선택자 #id | A B | #id A
+```html
+3.
+<not-element-just-cssdiner-title-3 class="Select-the-fancy-plate #id">하늘색 테두리 접시 1개 반응</not-element-just-cssdiner-title-3>
+
+<div class="table">
+<plate id="fancy" />
+<plate />
+<bento />
+</div>
+
+<not-element-just-cssdiner-answer-3 class="Select-the-fancy-plate #id">#fancy</not-element-just-cssdiner-answer-3>
+
+4.
+<not-element-just-cssdiner-title-4 class="Select-the-apple-on-the-plate A B">접시 위 사과 반응</not-element-just-cssdiner-title-4>
+
+<div class="table">
+<bento />
+<plate>
+  <apple />
+</plate>
+<apple />
+</div>
+
+<not-element-just-cssdiner-answer-4 class="Select-the-apple-on-the-plate A B">plate apple</not-element-just-cssdiner-answer-4>
+
+5.
+<not-element-just-cssdiner-title-5 class="Select-the-pickle-on-the-fancy-plate #id A">하늘색 테두리 접시 위 피클 반응</not-element-just-cssdiner-title-5>
+
+<div class="table">
+<bento>
+  <orange />
+</bento>
+<plate id="fancy">
+  <pickle />
+</plate>
+<plate>
+  <pickle />
+</plate>
+</div>
+
+<not-element-just-cssdiner-answer-5 class="Select-the-pickle-on-the-fancy-plate #id A">#fancy pickle</not-element-just-cssdiner-answer-5>
+
+
+
+
+
+
+```
+              </script>
+              <div class="toast-ui-viewer"></div>
+            </div>
+          </li>
+          <li>
+            <h1 class="title">1화, CSS DINER</h1>
+            <div class="reg-date">2020-01-12 12:12:14</div>
+            <div class="body">
+              <script type="text/x-template">
+# 선택자 A
+```html
+1.
+<not-element-just-cssdiner-title-1 class="Select-the-plates A">접시 2개 반응</not-element-just-cssdiner-title-1>
+
+<div class="table">
+<plate />
+<plate />
+</div>
+
+<not-element-just-cssdiner-answer-1 class="Select-the-plates A">plate</not-element-just-cssdiner-answer-1>
+
+2.
+<not-element-just-cssdiner-title-2 class="Select-the-bento-boxes A">도시락 박스 2개 반응</not-element-just-cssdiner-title-2>
+
+<div class="table">
+<bento />
+<plate />
+<bento />
+</div>
+
+<not-element-just-cssdiner-answer-2 class="Select-the-bento-boxes A">bento</not-element-just-cssdiner-answer-2>
+
+
+
+
+
+```
+              </script>
+              <div class="toast-ui-viewer"></div>
+            </div>
+          </li>
+        
+        </ul>
+      </div>
+    </div>
+  </section>
+
+<?php require_once "foot.php"; ?>
